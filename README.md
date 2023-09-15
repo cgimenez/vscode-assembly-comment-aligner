971113aee7af92fca3ddfb9b0cc8a53f15c8a272
Christophe Gimenez <christophe.gimenez@gmail.com>
2023-09-15 16:13:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Assembler comments aligner
+
+A quick and dirty extension for align comments on specific columns when editing assembly files (eg. ca65)
+Be warn that using this extension to any file without prior trying it with auto save disabled might lead to unpredictable results.
+
+The extension only aligns comments positionned at the end of a line.
+A comment preceded by only whitespaces will stay unmodified.
+
+Settings :
+
+- auto save : will align comments when file is saved
+- column : the column where
+- languages : the file language type to which align must be applied (eg. ca65)
+- the comment symbol (; being the default) : be careful with this one
+
+Install :
+
+- download the .vsix file from the release directory and install the extension with vs code
+
+**Enjoy! (or not)**
---
+++ extension.js
@@ -0,0 +1,78 @@
+
+const vscode = require('vscode');
+
+function formatComments(document, column_pos, comment_symbol) {
+  let new_content;
+  let new_lines = [];
+
+  for (let line_num = 0; line_num < document.lineCount; line_num++) {
+    let line = document.lineAt(line_num).text;
+    new_content = line;
+    if (line.length > 0) {
+      let regex = new RegExp("\^\s*" + comment_symbol);
+      let result = line.match(regex); // /^\s*;/ check for whitespaces followed by a comment symbol
+      if (!result) {
+        regex = new RegExp("\.+" + comment_symbol);
+        result = line.match(regex); // /.+;/ check for comment symbol
+        if (result) {
+          let pos = result[0].length;
+          let first_part = line.substring(0, pos - 1);
+          let padded;
+          if (pos > column_pos) { // need to shorten the line
+            first_part = first_part.slice(0, column_pos - 1); //
+            padded = "";
+          }
+          else
+            padded = " ".repeat(column_pos - pos);
+          let second_part = line.substring(pos - 1, line.length);
+          new_content = first_part + padded + second_part;
+        }
+      }
+    }
+    new_lines.push(new_content);
+  }
+  return new_lines.join("\n");
+}
+
+function activate(context) {
+  const configuration = vscode.workspace.getConfiguration('org-cg-comments-aligner')
+  const comment_symbol = configuration.comment_symbol;
+  let disposable1 = vscode.commands.registerCommand('org-cg-comments-aligner.align', function () {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
+    const document = editor.document;
+    if (!document)
+      return;
+    const fullRange = new vscode.Range(0, 0, document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+    const formattedText = formatComments(document, configuration.column, comment_symbol);
+    editor.edit((editBuilder) => {
+      editBuilder.replace(fullRange, formattedText);
+    });
+  });
+  context.subscriptions.push(disposable1);
+
+
+  if (configuration.align_onsave) {
+    const languages = configuration.languages;
+    let disposable2 = vscode.languages.registerDocumentFormattingEditProvider(languages, {
+      provideDocumentFormattingEdits: (document, options, token) => {
+        const fullRange = new vscode.Range(0, 0, document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+        const formattedText = formatComments(document, configuration.column, comment_symbol);
+        return [vscode.TextEdit.replace(fullRange, formattedText)];
+      }
+    });
+    context.subscriptions.push(disposable2);
+  }
+}
+
+function deactivate() { }
+
+module.exports = {
+  activate,
+  deactivate
+}
+
+//
+// cmd shift F5 pour recharger l'extension
+// cmd shift P pour lancer l'extension ou cmd R dans la seconde fenêtre
+//
